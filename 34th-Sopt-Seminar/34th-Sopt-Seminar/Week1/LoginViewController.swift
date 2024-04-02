788dd5a4d6736727c999b90ebeaaf51fc28c07fa
seuriseuljjeok <heeseul09@naver.com>
2024-04-02 21:00:02 +0900

    [feat] 1주차 세미나 실습 (#1)

---
+++ 34th-Sopt-Seminar/34th-Sopt-Seminar/Week1/LoginViewController.swift
@@ -0,0 +1,77 @@
+//
+//  LoginViewController.swift
+//  34th-Sopt-Seminar
+//
+//  Created by 윤희슬 on 4/2/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 69, y: 161, width: 236, height: 44))
+        label.text = "동네라서 가능한 모든것\n당근에서 가까운 이웃과 함께해요."
+        label.textColor = .black
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        return label
+    }()
+
+    private let idTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 20, y: 276, width: 335, height: 52))
+        textField.placeholder = "아이디를 입력해주세요"
+        textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+        return textField
+    }()
+
+    private let passwordTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 20, y: 335, width: 335, height: 52))
+        textField.placeholder = "비밀번호를 입력해주세요"
+        textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+        return textField
+    }()
+    
+    private lazy var loginButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 21, y: 422, width: 332, height: 58))
+        button.backgroundColor = UIColor(red: 255/255, green: 111/255, blue: 15/255, alpha: 1)
+        button.setTitle("로그인하기", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(loginButtonDidTap), for: .touchUpInside)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .white
+        setLayout()
+    }
+
+    private func setLayout() {
+        [titleLabel, idTextField, passwordTextField, loginButton].forEach {
+            self.view.addSubview($0)
+        }
+    }
+    
+    @objc
+    func loginButtonDidTap() {
+        presentToWelcomeVC()
+//        pushToWelcomeVC()
+    }
+    
+    private func presentToWelcomeVC() {
+        let welcomeVC = WelcomeViewController()
+        welcomeVC.modalPresentationStyle = .formSheet
+        welcomeVC.modalTransitionStyle = .flipHorizontal
+        welcomeVC.id = self.idTextField.text ?? ""
+        self.present(welcomeVC, animated: true)
+    }
+    
+    private func pushToWelcomeVC() {
+        let welcomeVC = WelcomeViewController()
+        self.navigationController?.pushViewController(welcomeVC, animated: true)
+    }
+    
+    
+}
---
+++ 34th-Sopt-Seminar/34th-Sopt-Seminar/Week1/WelcomeViewController.swift
@@ -0,0 +1,78 @@
+//
+//  WelcomeViewController.swift
+//  34th-Sopt-Seminar
+//
+//  Created by 윤희슬 on 4/2/24.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    private let rabbitImage: UIImageView = {
+        let img = UIImageView(frame: CGRect(x: 112, y: 87, width: 150, height: 150))
+        img.image = UIImage(resource: .danggeun)
+        return img
+    }()
+    
+    private let welcomeLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 140, y: 295, width: 95, height: 60))
+        label.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.text = "???님\n반가워요!"
+        return label
+    }()
+    
+    private lazy var mainButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 20, y: 426, width: 335, height: 58))
+        button.backgroundColor = UIColor(red: 1, green: 0.435, blue: 0.059, alpha: 1)
+        button.layer.cornerRadius = 6
+        button.setTitle("메인으로", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    private lazy var reloginButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 20, y: 498, width: 335, height: 58))
+        button.backgroundColor = UIColor(red: 0.867, green: 0.871, blue: 0.89, alpha: 1)
+        button.setTitle("다시 로그인", for: .normal)
+        button.setTitleColor(UIColor(red: 0.675, green: 0.69, blue: 0.725, alpha: 1), for: .normal)
+        button.layer.cornerRadius = 6
+        button.addTarget(self, action: #selector(backToLoginButtonDidTap), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    var id: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .white
+        
+        [rabbitImage, welcomeLabel, mainButton, reloginButton].forEach {
+            self.view.addSubview($0)
+        }
+        bingID()
+    }
+    
+    private func bingID() {
+        guard let idText = id else {return}
+        self.welcomeLabel.text = "\(idText)님\n반가워요!"
+
+//        if idText = id {
+//            self. welcomeLabel.text = "\(idText)님\n반가워요!"
+//        }
+    }
+    
+    @objc
+    private func backToLoginButtonDidTap() {
+        if self.navigationController == nil {
+            self.dismiss(animated: true)
+        } else {
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+
+}
