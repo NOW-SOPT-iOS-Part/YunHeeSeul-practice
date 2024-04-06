1e41be90fc630a81ee3f38fd7135ad4b45c183b4
seuriseuljjeok <heeseul09@naver.com>
2024-04-06 14:28:44 +0900

    [Add] 프로젝트 파일 추가

---
+++ 34th-Sopt-Seminar/34th-Sopt-Seminar/Week2/WelcomeViewController_DelegatePattern.swift
@@ -0,0 +1,109 @@
+//
+//  WelcomeViewController_DelegatePattern.swift
+//  34th-Sopt-Seminar
+//
+//  Created by 윤희슬 on 4/6/24.
+//
+
+import UIKit
+
+class WelcomeViewController_DelegatePattern: UIViewController {
+    
+    // MARK: - UI Properties
+    
+    private let rabbitImage: UIImageView = {
+        let img = UIImageView(frame: CGRect(x: UIScreen.main.bounds.width / 375 * 112,
+                                                            y: UIScreen.main.bounds.height / 812 * 87,
+                                                            width: UIScreen.main.bounds.width / 375 * 150,
+                                                            height: UIScreen.main.bounds.height / 812 * 150))
+        img.image = UIImage(resource: .danggeun)
+        return img
+    }()
+    
+    let welcomeLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: UIScreen.main.bounds.width / 375 * 140,
+                                                       y: UIScreen.main.bounds.height / 812 * 295,
+                                                       width: UIScreen.main.bounds.width / 375 * 105,
+                                                       height: UIScreen.main.bounds.height / 812 * 60))
+        label.textColor = UIColor(resource: .black)
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        label.text = "???님\n반가워요!"
+        label.font = UIFont.pretendard(.head1)
+        return label
+    }()
+    
+    private lazy var mainButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: UIScreen.main.bounds.width / 375 * 20,
+                                                          y: UIScreen.main.bounds.height / 812 * 426,
+                                                          width: UIScreen.main.bounds.width / 375 * 335,
+                                                          height: UIScreen.main.bounds.height / 812 * 58))
+        button.backgroundColor = UIColor(resource: .primaryOrange)
+        button.layer.cornerRadius = 6
+        button.setTitle("메인으로", for: .normal)
+        button.setTitleColor(UIColor(resource: .white), for: .normal)
+        button.addTarget(self, action: #selector(backToLoginButtonDidTap), for: .touchUpInside)
+        button.titleLabel?.font = UIFont.pretendard(.subhead1)
+        return button
+    }()
+    
+    lazy var reloginButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: UIScreen.main.bounds.width / 375 * 20,
+                                                          y: UIScreen.main.bounds.height / 812 * 498,
+                                                          width: UIScreen.main.bounds.width / 375 * 335,
+                                                          height: UIScreen.main.bounds.height / 812 * 58))
+        button.backgroundColor = UIColor(resource: .grey200)
+        button.setTitle("다시 로그인", for: .normal)
+        button.setTitleColor(UIColor(resource: .grey300), for: .normal)
+        button.layer.cornerRadius = 6
+        button.addTarget(self, action: #selector(backToLoginButtonDidTap), for: .touchUpInside)
+        button.titleLabel?.font = UIFont.pretendard(.subhead1)
+        return button
+    }()
+    
+    // MARK: - Properties
+    
+    var id: String?
+    
+    // MARK: - Life Cycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setLayout()
+        setStyle()
+        bingID()
+    }
+}
+
+
+// MARK: - Private Methods
+
+private extension WelcomeViewController_DelegatePattern {
+    
+    func setLayout() {
+        self.view.addSubviews(rabbitImage,
+                              welcomeLabel,
+                              mainButton,
+                              reloginButton)
+    }
+    
+    func setStyle() {
+        self.view.backgroundColor = .white
+    }
+    
+    func bingID() {
+        guard let idText = id else {return}
+        self.welcomeLabel.text = "\(idText)님\n반가워요!"
+    }
+    
+    @objc
+    func backToLoginButtonDidTap() {
+        if self.navigationController == nil {
+            self.dismiss(animated: true)
+        } else {
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+}
