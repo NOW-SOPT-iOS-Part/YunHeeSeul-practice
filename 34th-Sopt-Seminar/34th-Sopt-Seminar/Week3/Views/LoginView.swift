4564ac5804499e738561842a8492ea9cc613d258
seuriseuljjeok <heeseul09@naver.com>
2024-04-20 14:20:43 +0900

    [Add] then 패키지 추가

---
+++ 34th-Sopt-Seminar/34th-Sopt-Seminar/Week3/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  34th-Sopt-Seminar
+//
+//  Created by 윤희슬 on 4/20/24.
+//
+
+import UIKit
+
+class LoginView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
