14a5defc080baac864a79174e13537854d1c5c76
seuriseuljjeok <heeseul09@naver.com>
2024-04-20 15:45:29 +0900

    [Chore] 로그인 뷰 분리

--- 34th-Sopt-Seminar/34th-Sopt-Seminar/Week3/ViewControllers/LoginViewViewController.swift
+++ 34th-Sopt-Seminar/34th-Sopt-Seminar/Week3/ViewControllers/LoginViewViewController.swift
@@ -7,23 +7,117 @@
 
 import UIKit
 
-class LoginViewViewController: UIViewController {
+class LoginViewViewController: UIViewController {    
 
+    private let loginView = LoginView()
+    
+    var isActivate: Bool = false
+    
+    override func loadView() {
+        self.view = loginView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.loginView.delegate = self
+    }
+
+}
+
+extension LoginViewViewController: LoginViewDelegate {
+
+    func textFieldChange() {
+        let id = self.loginView.idTextField.text ?? ""
+        let pw = self.loginView.passwordTextField.text ?? ""
+        
+        if !id.isEmpty {
+            loginView.clearButton.isHidden = false
+        } else {
+            loginView.clearButton.isHidden = true
+        }
+        
+        if !id.isEmpty && !pw.isEmpty {
+            loginView.loginButton.backgroundColor = UIColor(resource: .primaryOrange)
+            loginView.loginButton.isEnabled = true
+            isActivate = true
+        } else {
+            loginView.loginButton.backgroundColor = loginView.switchButton.isOn ? UIColor(resource: .orange200) : UIColor(resource: .grey200)
+            loginView.loginButton.isEnabled = false
+            isActivate = false
+        }
     }
     
+    func maskButtonTapped() {
+        self.loginView.passwordTextField.isSecureTextEntry = !self.loginView.passwordTextField.isSecureTextEntry
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+    
+    func clearButtonTapped() {
+        self.loginView.idTextField.text = ""
+        loginView.clearButton.isHidden = true
+        loginView.loginButton.backgroundColor = loginView.switchButton.isOn ? UIColor(resource: .orange200) : UIColor(resource: .grey200)
+        loginView.loginButton.isEnabled = false
+        isActivate = false
+    }
+    
+    func onClickSwitch() {
+        self.view.backgroundColor = loginView.switchButton.isOn ? UIColor(resource: .black) : UIColor(resource: .white)
+        self.loginView.idTextField.backgroundColor = loginView.switchButton.isOn ? UIColor(resource: .orange200) : UIColor(resource: .grey200)
+        self.loginView.passwordTextField.backgroundColor = loginView.switchButton.isOn ? UIColor(resource: .orange200) : UIColor(resource: .grey200)
+        self.loginView.modeLabel.textColor = loginView.switchButton.isOn ? UIColor(resource: .white) : UIColor(resource: .black)
+        self.loginView.titleLabel.textColor = loginView.switchButton.isOn ? UIColor(resource: .white) : UIColor(resource: .black)
+        self.loginView.maskButton.tintColor = loginView.switchButton.isOn ? UIColor(resource: .black) : UIColor(resource: .grey300)
+        self.loginView.clearButton.tintColor = loginView.switchButton.isOn ? UIColor(resource: .black) : UIColor(resource: .grey300)
+        self.loginView.idTextField.setPlaceholder(placeholder: "아이디",
+                                                  fontColor: loginView.switchButton.isOn ? UIColor(resource: .white) : UIColor(resource: .grey300),
+                                        font: .pretendard(.subhead4))
+        self.loginView.passwordTextField.setPlaceholder(placeholder: "비밀번호",
+                                                        fontColor: loginView.switchButton.isOn ? UIColor(resource: .white) : UIColor(resource: .grey300),
+                                              font: .pretendard(.subhead4))
+        
+        if !isActivate {
+            self.loginView.loginButton.backgroundColor = loginView.switchButton.isOn ? UIColor(resource: .orange200) : UIColor(resource: .grey200)
+        } else {
+            self.loginView.loginButton.backgroundColor = UIColor(resource: .primaryOrange)
+        }
+    }
+    
+    func presentWelcomeVC() {
+        self.loginView.idTextField.resignFirstResponder()
+        self.loginView.passwordTextField.resignFirstResponder()
+        
+        let welcomeVC = WelcomeViewController_DelegatePattern()
+        welcomeVC.delegate = self
+        welcomeVC.modalPresentationStyle = .formSheet
+        welcomeVC.modalTransitionStyle = .flipHorizontal
+        welcomeVC.id = self.loginView.idTextField.text ?? ""
+        
+        if loginView.switchButton.isOn {
+            welcomeVC.welcomeLabel.textColor = UIColor(resource: .white)
+            welcomeVC.view.backgroundColor = UIColor(resource: .black)
+            welcomeVC.reloginButton.backgroundColor = UIColor(resource: .orange200)
+            welcomeVC.reloginButton.setTitleColor(UIColor(resource: .black), for: .normal)
+        } else {
+            welcomeVC.welcomeLabel.textColor = UIColor(resource: .black)
+            welcomeVC.view.backgroundColor = UIColor(resource: .white)
+        }
+        
+        self.present(welcomeVC, animated: true)
+    }
+    
+    func pushToWelcomeVC() {
+        let welcomeVC = WelcomeViewController_DelegatePattern()
+        welcomeVC.delegate = self
+        self.navigationController?.pushViewController(welcomeVC, animated: true)
+    }
+    
+}
 
+extension LoginViewViewController: DataBindProtocol {
+    func dataBind(id: String?) {
+        if let id = id {
+            loginView.idTextField.text = "\(id)는 뭘로 할꺼얌"
+        }
+    }
 }
--- 34th-Sopt-Seminar/34th-Sopt-Seminar/Week3/Views/LoginView.swift
+++ 34th-Sopt-Seminar/34th-Sopt-Seminar/Week3/Views/LoginView.swift
@@ -7,14 +7,296 @@
 
 import UIKit
 
-class LoginView: UIView {
+import Then
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+protocol LoginViewDelegate: AnyObject {
+    func presentWelcomeVC()
+    func pushToWelcomeVC()
+    func textFieldChange()
+    func maskButtonTapped()
+    func clearButtonTapped()
+    func onClickSwitch()    
 }
+
+final class LoginView: UIView {
+    
+    // MARK: - UI Properties
+    
+    let titleLabel = UILabel().then {
+        $0.text = "동네라서 가능한 모든것\n당근에서 가까운 이웃과 함께해요."
+        $0.textColor = UIColor(resource: .black)
+        $0.textAlignment = .center
+        $0.numberOfLines = 2
+        $0.font = UIFont.pretendard(.subhead1)
+    }
+    
+    lazy var idTextField =  UITextField().then {
+        $0.setPlaceholder(placeholder: "아이디",
+                                 fontColor: UIColor(resource: .grey300),
+                                 font: UIFont.pretendard(.subhead4))
+        $0.setTextField(forBackgroundColor: UIColor(resource: .grey200),
+                               forBorderColor: UIColor(resource: .grey200),
+                               forBorderWidth: 0,
+                               forCornerRadius: 3)
+        $0.setLeftPadding(amount: 23)
+        $0.addTarget(self, action: #selector(textFieldChange), for: .editingChanged)
+        
+        let rightView = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 30))
+        rightView.addSubview(clearButton)
+        $0.rightView = rightView
+        $0.rightViewMode = .always
+    }
+    
+    lazy var passwordTextField = UITextField().then {
+        $0.setPlaceholder(placeholder: "비밀번호",
+                                 fontColor: UIColor(resource: .grey300),
+                                 font: UIFont.pretendard(.subhead4))
+        $0.setTextField(forBackgroundColor: UIColor(resource: .grey200),
+                               forBorderColor: UIColor(resource: .grey200),
+                               forBorderWidth: 0,
+                               forCornerRadius: 3)
+        $0.setLeftPadding(amount: 23)
+        $0.addTarget(self, action: #selector(textFieldChange), for: .editingChanged)
+        $0.isSecureTextEntry = true
+        
+        let rightView = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 30))
+        rightView.addSubview(maskButton)
+        $0.rightView = rightView
+        $0.rightViewMode = .always
+    }
+    
+    lazy var maskButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "eye.slash.fill"), for: .normal)
+        $0.tintColor = UIColor(resource: .grey300)
+        $0.addTarget(self, action: #selector(maskButtonTapped), for: .touchUpInside)
+    }
+    
+    lazy var clearButton = UIButton().then {
+        $0.isHidden = true
+        $0.setImage(UIImage(systemName: "multiply.circle.fill"), for: .normal)
+        $0.tintColor = UIColor(resource: .grey300)
+        $0.addTarget(self, action: #selector(clearButtonTapped), for: .touchUpInside)
+    }
+    
+    lazy var loginButton = UIButton().then {
+        $0.backgroundColor = UIColor(resource: .grey200)
+        $0.setTitle("로그인하기", for: .normal)
+        $0.setTitleColor(UIColor(resource: .white), for: .normal)
+        $0.addTarget(self, action: #selector(presentToWelcomeVC), for: .touchUpInside)
+        $0.titleLabel?.font = UIFont.pretendard(.subhead1)
+        $0.layer.cornerRadius = 6
+        $0.isEnabled = false
+    }
+    
+    let modeLabel = UILabel().then {
+        $0.text = "mode"
+        $0.textColor = UIColor(resource: .black)
+        $0.textAlignment = .center
+        $0.numberOfLines = 2
+        $0.font = UIFont.pretendard(.subhead1)
+    }
+    
+    lazy var switchButton = UISwitch().then {
+        $0.onTintColor = UIColor(resource: .orange200)
+        $0.tintColor = UIColor(resource: .black)
+        $0.isOn = false
+        $0.addTarget(self, action: #selector(onClickSwitch), for: .valueChanged)
+    }
+    
+    // MARK: - UI Properties
+    
+    var isActivate: Bool = false
+    
+    weak var delegate: LoginViewDelegate?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+
+
+
+// MARK: - Private Methods
+
+private extension LoginView {
+    
+    func setLayout() {
+        self.addSubviews(titleLabel,
+                              idTextField,
+                              passwordTextField,
+                              loginButton,
+                              switchButton,
+                              modeLabel)
+        
+        idTextField.addSubviews(clearButton)
+        passwordTextField.addSubviews(maskButton)
+        
+        modeLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(97)
+            $0.leading.equalToSuperview().inset(142)
+            $0.height.equalTo(30)
+            $0.width.equalTo(60)
+        }
+        
+        switchButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(97)
+            $0.leading.equalTo(modeLabel.snp.trailing).offset(10)
+            $0.height.equalTo(30)
+            $0.width.equalTo(40)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(modeLabel.snp.bottom).offset(70)
+            $0.width.equalTo(236)
+            $0.centerX.equalToSuperview()
+        }
+        
+        idTextField.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(70)
+            $0.width.equalTo(335)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(50)
+        }
+        
+        passwordTextField.snp.makeConstraints {
+            $0.top.equalTo(idTextField.snp.bottom).offset(7)
+            $0.width.equalTo(335)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(50)
+        }
+        
+        loginButton.snp.makeConstraints {
+            $0.top.equalTo(passwordTextField.snp.bottom).offset(35)
+            $0.width.equalTo(335)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(60)
+        }
+        
+        maskButton.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(20)
+        }
+        
+        clearButton.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(20)
+        }
+        
+    }
+    
+    func setStyle() {
+        self.backgroundColor = .white
+    }
+    
+    func setDelegate() {
+        self.idTextField.delegate = self
+        self.passwordTextField.delegate = self
+    }
+    
+    @objc 
+    func presentToWelcomeVC() {
+        self.delegate?.presentWelcomeVC()
+    }
+    
+    func pushToWelcomeVC() {
+        self.delegate?.pushToWelcomeVC()
+    }
+    
+    @objc
+    func textFieldChange() {
+        let id = self.idTextField.text ?? ""
+        let pw = self.passwordTextField.text ?? ""
+        
+        if !id.isEmpty {
+            clearButton.isHidden = false
+        } else {
+            clearButton.isHidden = true
+        }
+        
+        if !id.isEmpty && !pw.isEmpty {
+            loginButton.backgroundColor = UIColor(resource: .primaryOrange)
+            loginButton.isEnabled = true
+            isActivate = true
+        } else {
+            loginButton.backgroundColor = switchButton.isOn ? UIColor(resource: .orange200) : UIColor(resource: .grey200)
+            loginButton.isEnabled = false
+            isActivate = false
+        }
+    }
+    
+    @objc
+    func maskButtonTapped() {
+        self.passwordTextField.isSecureTextEntry = !self.passwordTextField.isSecureTextEntry
+    }
+    
+    @objc
+    func clearButtonTapped() {
+        self.idTextField.text = ""
+        clearButton.isHidden = true
+        loginButton.backgroundColor = switchButton.isOn ? UIColor(resource: .orange200) : UIColor(resource: .grey200)
+        loginButton.isEnabled = false
+        isActivate = false
+    }
+    
+    @objc
+    func onClickSwitch() {
+        self.backgroundColor = switchButton.isOn ? UIColor(resource: .black) : UIColor(resource: .white)
+        self.idTextField.backgroundColor = switchButton.isOn ? UIColor(resource: .orange200) : UIColor(resource: .grey200)
+        self.passwordTextField.backgroundColor = switchButton.isOn ? UIColor(resource: .orange200) : UIColor(resource: .grey200)
+        self.modeLabel.textColor = switchButton.isOn ? UIColor(resource: .white) : UIColor(resource: .black)
+        self.titleLabel.textColor = switchButton.isOn ? UIColor(resource: .white) : UIColor(resource: .black)
+        self.maskButton.tintColor = switchButton.isOn ? UIColor(resource: .black) : UIColor(resource: .grey300)
+        self.clearButton.tintColor = switchButton.isOn ? UIColor(resource: .black) : UIColor(resource: .grey300)
+        self.idTextField.setPlaceholder(placeholder: "아이디",
+                                        fontColor: switchButton.isOn ? UIColor(resource: .white) : UIColor(resource: .grey300),
+                                        font: .pretendard(.subhead4))
+        self.passwordTextField.setPlaceholder(placeholder: "비밀번호",
+                                              fontColor: switchButton.isOn ? UIColor(resource: .white) : UIColor(resource: .grey300),
+                                              font: .pretendard(.subhead4))
+        
+        if !isActivate {
+            self.loginButton.backgroundColor = switchButton.isOn ? UIColor(resource: .orange200) : UIColor(resource: .grey200)
+        } else {
+            self.loginButton.backgroundColor = UIColor(resource: .primaryOrange)
+        }
+        
+    }
+    
+}
+
+
+// MARK: - Delegates
+
+extension LoginView: UITextFieldDelegate {
+    func textFieldDidBeginEditing (_ textField: UITextField) {
+        
+        if textField.placeholder == "아이디" {
+            self.idTextField.layer.borderWidth = 2
+            self.idTextField.layer.borderColor = UIColor(resource: .grey500).cgColor
+        } else {
+            self.passwordTextField.layer.borderWidth = 2
+            self.passwordTextField.layer.borderColor = UIColor(resource: .grey500).cgColor
+        }
+        
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if textField.placeholder == "아이디" {
+            self.idTextField.layer.borderWidth = 0
+            self.idTextField.layer.borderColor = UIColor(resource: .grey200).cgColor
+        } else {
+            self.passwordTextField.layer.borderWidth = 0
+            self.passwordTextField.layer.borderColor = UIColor(resource: .grey200).cgColor
+        }
+    }
+}
+
